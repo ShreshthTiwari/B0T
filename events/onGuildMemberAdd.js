b2602899bbbcbd93f78c0194d2e801d0dc00d505
diyao9 <diyapandeypvt.o.9@gmail.com>
2022-02-22 09:07:16 +0530

    updates

--- events/onGuildMemberAdd.js
+++ events/onGuildMemberAdd.js
@@ -1,6 +1,5 @@
 module.exports = async(Discord, Keyv, member, Canvas, path, database) =>{
   let embed = new Discord.MessageEmbed()
-    .setAuthor(member.guild.name, member.guild.iconURL())
     .setColor(0xff4747);
   let moderateNewUserNames = await database.get("moderateNewUserNames");
   if(!moderateNewUserNames){
@@ -24,20 +23,22 @@ module.exports = async(Discord, Keyv, member, Canvas, path, database) =>{
       await member.setNickname(`moderated-${++count}`);
       let newName = member.displayName;
       await gDB.set("illegalNamesCount", count);
-      embed.setDescription(`Your name [${oldName}] contains illegal characters.\nSo i have changed it to [${newName}].\nKindly ask a server staff to change it now.`);
+      embed.setAuthor(member.guild.name, member.guild.iconURL())
+      .setDescription(`Your name [${oldName}] contains illegal characters.\nSo i have changed it to [${newName}].\nKindly ask a server staff to change it now.`);
       await member.send(embed).catch(error => {});
     }
   }
 
   let memberJoinLogsChannelID = await database.get("memberJoinLogsChannelID");
   if(memberJoinLogsChannelID){
-    let memberJoinLogsChannel = await database.get("memberJoinLogsChannelID");
+    let memberJoinLogsChannel = await member.guild.channels.cache.get(memberJoinLogsChannelID);
     if(memberJoinLogsChannel){
-      embed.setTitle("New Member Joined")
-        .setDescription(`**User**- ${message.author}.
-        **Name**- \`${message.author.tag}\`.
-        **ID**- \`${message.author.id}\`.`)
-        .setThumbnail(person.displayAvatarURL())
+      embed = new Discord.MessageEmbed()
+        .setTitle("New Member Joined")
+        .setDescription(`**User**- ${member.user}.
+        **Name**- \`${member.user.tag}\`.
+        **ID**- \`${member.user.id}\`.`)
+        .setThumbnail(member.user.displayAvatarURL())
         .setColor(0x95fd91);
       await memberJoinLogsChannel.send(embed).catch(error => {});
     }
