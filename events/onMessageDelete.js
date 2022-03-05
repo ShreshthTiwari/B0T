db01f49544f82af22f8e55f7005956caaf96e44d
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2022-03-05 20:21:52 +0530

    Update onMessageDelete.js

--- events/onMessageDelete.js
+++ events/onMessageDelete.js
@@ -8,21 +8,21 @@ module.exports = async (Discord, client, deletedMessage, database) => {
     chatLogsChannel = await deletedMessage.guild.channels.cache.get(chatLogsChannelID);
     if(chatLogsChannel){
       let embed = new Discord.MessageEmbed()
-        .setColor(0x98dbfa);
+      .setColor(0x98dbfa);
       let content = deletedMessage.content;
       if(content.length > 100){
         content.length = 97;
         content = content + "...";
       }
       embed = new Discord.MessageEmbed()
-        .setTitle("Message Deleted")
-        .setDescription(`**User**- ${deletedMessage.author}.
-        **Name**- \`${deletedMessage.author.tag}\`.
-        **ID**- \`${deletedMessage.author.id}\`.
-        **Channel**- ${deletedMessage.channel}.
-        **Message ID**- \`${deletedMessage.id}\`.
-        **Content**-\n${content}`)
-        .setColor(0x95fd91);
+      .setTitle("Message Deleted")
+      .setDescription(`**User**- ${deletedMessage.author}.
+      **Name**- \`${deletedMessage.author.tag}\`.
+      **ID**- \`${deletedMessage.author.id}\`.
+      **Channel**- ${deletedMessage.channel}.
+      **Message ID**- \`${deletedMessage.id}\`.
+      **Content**-\n${content}`)
+      .setColor(0x95fd91);
       await chatLogsChannel.send(embed).catch(error => {});
     }
   }
