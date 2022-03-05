6768b60a222517527e4c64063ab08cb221b40ea2
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2022-03-05 20:22:13 +0530

    Update chatFilter.js

--- events/chatFilter.js
+++ events/chatFilter.js
@@ -11,8 +11,8 @@ module.exports = async(Discord, client, message, content, database, emojiIDs) =>
       return;
     }
     let chatFilterEmbed = new Discord.MessageEmbed()
-      .setTitle("Chat Filter Log")
-      .setColor(0x98dbfa);
+    .setTitle("Chat Filter Log")
+    .setColor(0x98dbfa);
     let swearsList = await database.get('swearsList');
     let badwordsList = await database.get("badwordsList");
     if((!swearsList) && (!badwordsList)){
@@ -25,11 +25,11 @@ module.exports = async(Discord, client, message, content, database, emojiIDs) =>
           if(content.includes(swearings[i])){
             await message.reply(`${cross} No swearing bruh.`).then((msg) => setTimeout(function(){msg.delete().catch(error => {});}, 10000)).catch(error => {});
             chatFilterEmbed.setDescription(`
-              **User**- ${message.author.tag}\n
-              **ID**- ${message.author.id}\n
-              **Message**- ${message.content}\n
-              **Filtered**- ${swearings[i]}\n
-              **Result**- Deleted`);
+            **User**- ${message.author.tag}\n
+            **ID**- ${message.author.id}\n
+            **Message**- ${message.content}\n
+            **Filtered**- ${swearings[i]}\n
+            **Result**- Deleted`);
             await chatFilterLogsChannel.send(chatFilterEmbed).catch(error => {});
             await message.delete().catch(error => {});
             return;
@@ -47,11 +47,11 @@ module.exports = async(Discord, client, message, content, database, emojiIDs) =>
               message.react("🇺")
             );
             chatFilterEmbed.setDescription(`
-              **User**- ${message.author.tag}\n
-              **ID**- ${message.author.id}\n
-              **Message**- ${message.content}\n
-              **Filtered**- ${badwords[i]}\n
-              **Result**- Reacted with 🇳 🇴 🇺`);
+            **User**- ${message.author.tag}\n
+            **ID**- ${message.author.id}\n
+            **Message**- ${message.content}\n
+            **Filtered**- ${badwords[i]}\n
+            **Result**- Reacted with 🇳 🇴 🇺`);
             await chatFilterLogsChannel.send(chatFilterEmbed).catch(error => {});
             return;
           }
